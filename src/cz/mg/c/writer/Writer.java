9d6d80c877393a86a81eb49b718268987baa4097
mg <gekoncze@seznam.cz>
2024-10-13 16:48:58 +0200

    removed obsolete classes

--- src/cz/mg/c/writer/Writer.java
+++ src/cz/mg/c/writer/Writer.java
@@ -2,8 +2,8 @@ package cz.mg.c.writer;
 
 import cz.mg.annotations.classes.Service;
 import cz.mg.annotations.requirement.Mandatory;
-import cz.mg.c.writer.services.HeaderFileFormatter;
-import cz.mg.c.writer.services.SourceFileFormatter;
+import cz.mg.c.entities.CFile;
+import cz.mg.c.writer.services.FileFormatter;
 import cz.mg.file.page.Page;
 import cz.mg.file.page.PageWriter;
 
@@ -16,8 +16,7 @@ public @Service class Writer {
                 if (instance == null) {
                     instance = new Writer();
                     instance.writer = PageWriter.getInstance();
-                    instance.headerFileFormatter = HeaderFileFormatter.getInstance();
-                    instance.sourceFileFormatter = SourceFileFormatter.getInstance();
+                    instance.fileFormatter = FileFormatter.getInstance();
                 }
             }
         }
@@ -25,23 +24,13 @@ public @Service class Writer {
     }
 
     private @Service PageWriter writer;
-    private @Service HeaderFileFormatter headerFileFormatter;
-    private @Service SourceFileFormatter sourceFileFormatter;
+    private @Service FileFormatter fileFormatter;
 
-    public void write(@Mandatory CHeaderFile headerFile) {
+    public void write(@Mandatory CFile file) {
         writer.write(
             new Page(
-                headerFile.getPath(),
-                headerFileFormatter.format(headerFile)
-            )
-        );
-    }
-
-    public void write(@Mandatory CSourceFile sourceFile) {
-        writer.write(
-            new Page(
-                sourceFile.getPath(),
-                sourceFileFormatter.format(sourceFile)
+                file.getPath(),
+                fileFormatter.format(file)
             )
         );
     }
